ff61c6e175dd8106827fe02ad3a50e325ef52c9b
verymew <verymewfor@gmail.com>
2024-08-08 17:50:24 -0300

    Lógica de registro iniciada

--- src/app/pages/authpage/authpage.component.ts
+++ src/app/pages/authpage/authpage.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -7,6 +7,8 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 
 
 
@@ -15,15 +17,19 @@ import {MatDividerModule} from '@angular/material/divider';
 @Component({
   selector: 'app-authpage',
   standalone: true,
-  imports: [MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatFormField, MatIconModule, MatInputModule, MatButtonModule, MatDividerModule],
+  imports: [MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatFormField, MatIconModule, MatInputModule, MatButtonModule, MatDividerModule, CommonModule],
   templateUrl: './authpage.component.html',
   styleUrl: './authpage.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AuthpageComponent {
-  hide = signal(true);
+  public hide = signal(true);
+  public isLogin:Boolean = true;
+  private auth = inject(AuthService);
 
-  constructor(){}
+  constructor()
+  {
+  }
 
   registration = new FormGroup({
     email : new FormControl(''),
@@ -33,6 +39,24 @@ export class AuthpageComponent {
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
+  }
+
+  public registerNewUser(): void
+  {
+    const email = this.registration.get('email')?.value;
+    const password = this.registration.get('password')?.value;
+
+    if(email && password)
+    {
+      this.auth.registerUser(email.trim(), password?.trim());
+      return alert('Registrado');
+    }
+
+  }
+
+  public changeStateLogin()
+  {
+    this.isLogin = !this.isLogin;
   }
 
 
