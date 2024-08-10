923647763359f8624b9adbfc31fc966798e1c019
verymew <verymewfor@gmail.com>
2024-08-10 17:25:28 -0300

    autenticação de rotas adicionada

--- src/app/components/require-auth/require-auth.component.spec.ts
+++ src/app/components/require-auth/require-auth.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { RequireAuthComponent } from './require-auth.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('RequireAuthComponent', () => {
+  let component: RequireAuthComponent;
+  let fixture: ComponentFixture<RequireAuthComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [RequireAuthComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(RequireAuthComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
