108af18d05b4cb748cecd37970491843739a9a72
verymew <verymewfor@gmail.com>
2024-08-10 02:13:29 -0300

    Profile page adicionada

--- src/app/pages/profilepage/profilepage.component.ts
+++ src/app/pages/profilepage/profilepage.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-profilepage',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatSidenavModule, MatExpansionModule, MatDividerModule, MatIconModule  ],
   templateUrl: './profilepage.component.html',
   styleUrl: './profilepage.component.css'
 })
 export class ProfilepageComponent {
+  readonly panelOpenState = signal(false);
+  public userName:string = "Julia";
 
 }
