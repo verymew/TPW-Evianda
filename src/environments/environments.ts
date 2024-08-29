247547f435518aa508497e51b0b6293f96abe2f6
verymew <verymewfor@gmail.com>
2024-08-28 23:45:00 -0300

    firebaseconfig Omitido

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,10 +1,5 @@
 export const env = {
   firebaseConfig: {
-    apiKey: "AIzaSyC-P4zO5Wx1X5DnEitTTs_YWX7ZtuFxC-k",
-    authDomain: "mula-139ac.firebaseapp.com",
-    projectId: "mula-139ac",
-    storageBucket: "mula-139ac.appspot.com",
-    messagingSenderId: "516757730836",
-    appId: "1:516757730836:web:aecee1cfb7343072731901"
+
   }
 };
